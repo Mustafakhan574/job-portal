019a3ff92975e714293ab0d039d1bafac4188846
Mustafakhan574 <mk3855315@gmail.com>
2026-01-22 15:28:55 +0530

    Update main.jsx

--- admin/src/main.jsx
+++ admin/src/main.jsx
@@ -3,7 +3,7 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import {BrowserRouter} from 'react-router-dom'
-import Authapi from '../Context/authapi.jsx'
+import Authapi from '../Context/Authapi.jsx'
 createRoot(document.getElementById('root')).render(
   <BrowserRouter>
   <Authapi>
