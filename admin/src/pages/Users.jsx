48b984b3cd2cae70a8a2fe96101e8912421e3f0a
Mustafakhan574 <mk3855315@gmail.com>
2026-02-07 11:56:39 +0530

    Update Users.jsx

--- admin/src/pages/Users.jsx
+++ admin/src/pages/Users.jsx
@@ -8,10 +8,10 @@ const Seeapplyjobs = () => {
   let navigate = useNavigate()
   let {usersaccounts} = useContext(authdata)
   return (
-    <div className='w-full min-h-screen bg-[#540505]  flex flex-col items-center gap-4 text-white mb-[5px]'>
+    <div className='w-full min-h-screen bg-[#540505]  flex flex-col items-center gap-4 text-white '>
           <BsBoxArrowLeft className='text-[30px] text-[white]  fixed left-0' onClick={()=>navigate("/")}/>
      <h1 className='text-white text-[40px] text-center pt-[30px]'>Applied Jobs</h1>
-     <div className='w-[98%] flex flex-col sm:w-[70%]'>
+     <div className='w-[98%] flex flex-col sm:w-[70%] mb-5'>
         {usersaccounts && usersaccounts.map((user)=>(
           <div className='flex w-[100%] border justify-between py-5 px-4 mb-5 text-black bg-white rounded-lg'> 
           <p className='text-[15px]  font-medium sm:text-[25px]'>{user.name}</p>
